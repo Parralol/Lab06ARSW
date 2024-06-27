ff2f30e31c204045e13855ad9670dc94ae7b3032
Parralol <110953563+Parralol@users.noreply.github.com>
2024-06-27 12:08:20 -0500

    Add files via upload

---
+++ src/main/java/edu/escuelaing/arsw/ase/app/lab06/controller/Lab06Controller.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.arsw.ase.app.lab06.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Lab06Controller {
+    
+}
---
+++ src/test/java/edu/escuelaing/arsw/ase/app/lab06/Lab06ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.ase.app.lab06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
